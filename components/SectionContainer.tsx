31aafff012d134a331530640489f8645dc585b3d
totuslink <summerbud@totuslink.com>
2021-08-17 15:26:44 +0800

    🔧 fix(code style): fix some code style issue

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
 
-interface Props {}
+interface Props { }
 
 const SectionContainer: React.FC<Props> = ({ children }) => {
   return (
-    <div
-      className="flex flex-col m-auto max-w-4xl lg:max-w-5xl px-8 lg:px-4"
-    >
+    <div className="flex flex-col m-auto max-w-4xl lg:max-w-[1200px] px-8 lg:px-4">
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default SectionContainer
+export default SectionContainer;
